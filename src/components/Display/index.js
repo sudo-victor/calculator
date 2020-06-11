48596cb5e5652dadbdedc3cebfada350782b74bc
Victor de Souto <victor.souto@hotmail.com>
2020-06-11 12:47:36 -0400

    chore: remove logs

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -5,7 +5,6 @@ import { Container, Input } from "./styles";
 
 export default function Display() {
     const calculator = useSelector((state) => state.calculatorReducer);
-    //let display = `${calculator.calculated} | ${calculator.operator} | ${calculator.currentValue} | ${calculator.display}`;
     const display = calculator.display;
     return (
         <Container>
