999967e931fd83add5d8968ee11f863b798dadd3
Victor de Souto <victor.souto@hotmail.com>
2020-06-11 11:56:32 -0400

    chore: add power button

--- src/components/Keyboard/index.js
+++ src/components/Keyboard/index.js
@@ -9,27 +9,27 @@ export default function Keyboard() {
             <Button value={"C"} type={"action"} />
             <Button value={"AC"} type={"action"} />
             <Button value={"+/-"} type={"action"} />
-            <Button value={"/"} type={"operator"} />
+            <Button value={"On/Off"} type={"action"} />
 
             <Button value={7} type={"number"} />
             <Button value={8} type={"number"} />
             <Button value={9} type={"number"} />
-            <Button value={"*"} type={"operator"} />
+            <Button value={"/"} type={"operator"} />
 
             <Button value={4} type={"number"} />
             <Button value={5} type={"number"} />
             <Button value={6} type={"number"} />
-            <Button value={"-"} type={"operator"} />
+            <Button value={"*"} type={"operator"} />
 
             <Button value={3} type={"number"} />
             <Button value={2} type={"number"} />
             <Button value={1} type={"number"} />
-            <Button value={"+"} type={"operator"} />
+            <Button value={"-"} type={"operator"} />
 
-            <Button value={"."} type={"action"} />
+            <Button value={"."} type={"number"} />
             <Button value={0} type={"number"} />
-            <Button value={""} />
             <Button value={"="} type={"operator"} />
+            <Button value={"+"} type={"operator"} />
         </Container>
     );
 }
